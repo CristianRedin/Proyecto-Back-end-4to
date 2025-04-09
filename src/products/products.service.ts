1d5e7b7a7a813bc30efa13e3ee8a49905d7e5027
CristianRedin <kristianredin@gmail.com>
2025-04-09 09:53:03 -0500

    Avances

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,25 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { Product } from './interfaces/product/product.interface';
 
 @Injectable()
 export class ProductsService {
-    private products = [
+    private products: Product[] = [
         {
-        id: 1,
+        id: 0,
         name: 'Marco de fotos pequeño',
         description: 'Marco ideal para foto de 10x10'
     },
     {
-        id: 2,
+        id: 1,
         name: 'Marco de fotos Mediano',
         description: 'Marco ideal para foto de 20x20'
     },
     {
-        id: 3,
+        id: 2,
         name: 'Marco de fotos Grande',
         description: 'Marco ideal para foto de 50x40'
     },
     {
-        id: 4,
+        id: 3,
         name: 'Vela aromática',
         description: 'Esta vela lanza ricos olores',
       },
